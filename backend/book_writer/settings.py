622cd99cb68b27ba66acb15dfef2b50397b1dc12
Dominik Szopa <dszopa@gmail.com>
2023-09-25 21:58:24 +0200

    Added cors

--- backend/book_writer/settings.py
+++ backend/book_writer/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'sections',
 ]
 
@@ -39,6 +40,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'book_writer.urls'
